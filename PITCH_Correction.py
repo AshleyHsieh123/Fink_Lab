17c113c1ab04bc8bd1d9a7e5ab9659f7bfe19c74
AshleyHsieh123 <ashley.hsieh@northwestern.edu>
2025-03-27 11:30:51 -0500

    Update PITCH_Correction.py

--- PITCH_Correction.py
+++ PITCH_Correction.py
@@ -209,6 +209,7 @@ def plot_histogram(mousedata):
 
     display(Javascript(f'''
     var plotBox = document.getElementById("plotBox");
+    plotBox.innerHTML = '';  // Clear previous plot
     plotBox.innerHTML = '<img src="data:image/png;base64,' + "{img_base64}" + '" />';
     '''))
 
