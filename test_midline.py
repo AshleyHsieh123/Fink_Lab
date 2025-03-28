88255ac1d2643466b8a07220c56f934173f5855c
AshleyHsieh123 <ashley.hsieh@northwestern.edu>
2025-03-28 16:05:39 -0500

    Update test_midline.py

--- test_midline.py
+++ test_midline.py
@@ -236,12 +236,12 @@ def finish_correction():
     worksheet.clear()  # Optional: Use with caution, can clear the entire sheet
     set_with_dataframe(worksheet, head_parameter)  # Update the sheet
 
-    mice = np.array(file.iloc[0,:])
+    mice = np.array(file_id.iloc[0,:])
     num_mice = len(mice[60:]) # count the number of mice
 
     mouse_index = np.where(mice == mouse_id)[0][0]  # Find the index of the mouse
-    mousefile = file.iloc[0:53,mouse_index]
-    mousefile.index = file.iloc[0:53,1].to_list()
+    mousefile = file_id.iloc[0:53,mouse_index]
+    mousefile.index = file_id.iloc[0:53,1].to_list()
     mouseData1 = [[mousefile['Weight before surgery (g)'],mousefile['Left ear bar (initial) (mm)'],mousefile['Right ear bar (initial) (mm)'],mousefile['Nose DV position º'],mousefile['RCS-lambda distance (µm)']]]
     mouseData2 = [[mousefile['At 1000PRCS, L positions of LEFT temporal ridge (µm)'],mousefile['At 1000PRCS, L positions of RIGHT temporal ridge (µm)']],
                   [mousefile['At 1500PRCS, L positions of LEFT temporal ridge (µm)'],mousefile['At 1500PRCS, L positions of RIGHT temporal ridge (µm)']],
@@ -262,20 +262,20 @@ def finish_correction():
 
      # All previous MetaData
 
-    leftRidge = file.iloc[9:17,60:]
-    leftRidge.columns = file.iloc[0,60:]
-    rightRidge = file.iloc[17:25,60:]
-    rightRidge.columns = file.iloc[0,60:]
-    leftRidgeZ = file.iloc[25:33,60:]
-    leftRidgeZ.columns = file.iloc[0,60:]
-    rightRidgeZ = file.iloc[33:41,60:]
-    rightRidgeZ.columns = file.iloc[0,60:]
+    leftRidge = file_id.iloc[9:17,60:]
+    leftRidge.columns = file_id.iloc[0,60:]
+    rightRidge = file_id.iloc[17:25,60:]
+    rightRidge.columns = file_id.iloc[0,60:]
+    leftRidgeZ = file_id.iloc[25:33,60:]
+    leftRidgeZ.columns = file_id.iloc[0,60:]
+    rightRidgeZ = file_id.iloc[33:41,60:]
+    rightRidgeZ.columns = file_id.iloc[0,60:]
 
-    animalWeight = file.iloc[0,60:]
-    LeftEarBarInitial = file.iloc[3,60:]
-    RightEarBarInitial = file.iloc[4,60:]
-    NoseDVposition = file.iloc[8,60:]
-    RCSlambdaDistance = file.iloc[7,60:]
+    animalWeight = file_id.iloc[0,60:]
+    LeftEarBarInitial = file_id.iloc[3,60:]
+    RightEarBarInitial = file_id.iloc[4,60:]
+    NoseDVposition = file_id.iloc[8,60:]
+    RCSlambdaDistance = file_id.iloc[7,60:]
 
     # Functions
 
