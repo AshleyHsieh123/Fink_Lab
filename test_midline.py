8c317f07a18c99d806fec7d4c6a3026f93d66732
AshleyHsieh123 <ashley.hsieh@northwestern.edu>
2025-03-28 12:31:39 -0500

    Update test_midline.py

--- test_midline.py
+++ test_midline.py
@@ -182,16 +182,16 @@ def update_correction_result(val1, val2, val3, val4, val5, val6, val7, val8, val
             float(val1),  # xL1500
             float(val2),  # xL2000
             float(val3),  # xL2500
-            float(head_parameter['xL1000'][0]),  # xL1000 from the sheet
-            float(head_parameter['xL3000'][0])   # xL3000 from the sheet
+            float(head_parameter.iloc[9,-1]),  # xL1000 from the sheet
+            float(head_parameter.iloc[13,-1])   # xL3000 from the sheet
         ]
         
         xR_values = [
             float(val4),  # xR1500
             float(val5),  # xR2000
             float(val6),  # xR2500
-            float(head_parameter['xR1000'][0]),  # xR1000 from the sheet
-            float(head_parameter['xR3000'][0])   # xR3000 from the sheet
+            float(head_parameter.iloc[17,-1]),  # xR1000 from the sheet
+            float(head_parameter.iloc[21,-1])   # xR3000 from the sheet
         ]
 
         differences = []
