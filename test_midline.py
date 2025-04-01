c0b27845ae27a275655381f37cdcc0d749e8be0e
AshleyHsieh123 <ashley.hsieh@northwestern.edu>
2025-04-01 17:47:02 -0500

    Update test_midline.py

--- test_midline.py
+++ test_midline.py
@@ -81,7 +81,7 @@ def create_input_boxes():
     var plotBox1 = document.createElement("div");
     plotBox1.id = "plotBox1";
     plotBox1.style.margin = "10px";
-    plotBox1.style.height = "1500";  // Adjusted height for better image display
+    plotBox1.style.height = "1500px";  // Adjusted height for better image display
     plotBox1.style.width = "80%";
     plotBox1.style.display = "block";
     plotBox1.style.backgroundColor = "#ffe0e0";  // Light red-pink for Plot 1
@@ -89,7 +89,7 @@ def create_input_boxes():
     var plotBox2 = document.createElement("div");
     plotBox2.id = "plotBox2";
     plotBox2.style.margin = "10px";
-    plotBox2.style.height = "1500";  // Adjusted height for better image display
+    plotBox2.style.height = "1500px";  // Adjusted height for better image display
     plotBox2.style.width = "80%";
     plotBox2.style.display = "block";
     plotBox2.style.backgroundColor = "#e0f7ff";  // Light blue for Plot 2
@@ -97,7 +97,7 @@ def create_input_boxes():
     var plotBox3 = document.createElement("div");
     plotBox3.id = "plotBox3";
     plotBox3.style.margin = "10px";
-    plotBox3.style.height = "1500";  // Adjusted height for better image display
+    plotBox3.style.height = "1500px";  // Adjusted height for better image display
     plotBox3.style.width = "80%";
     plotBox3.style.display = "block";
     plotBox3.style.backgroundColor = "#e9ffe0";  // Light green for Plot 3
@@ -605,8 +605,6 @@ def update_figure_1():
     img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
     
     display_inline_image(img_base64, "plotBox1")
-    plt.close(fig1)
-    print('\n\n')
 
 # Figure 2, histograms showing the L-R x-positions
 def update_figure_2():
@@ -645,8 +643,6 @@ def update_figure_2():
     img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
 
     display_inline_image(img_base64, "plotBox2")
-    plt.close(fig2)
-    print('\n\n')
 
 # Figure 3, histograms showing the L-R z-positions
 def update_figure_3():
@@ -683,13 +679,7 @@ def update_figure_3():
     fig3.savefig(img_buf, format='png')
     img_buf.seek(0)
     img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
-
     display_inline_image(img_base64, "plotBox3")
-    plt.close(fig3)
-    print('\n\n')
-
-    print("Figures saved and encoded in base64.")
-    print("JavaScript for embedding images executed.")    
 
 def finish_correction():
     print("Finish correction button clicked!")
