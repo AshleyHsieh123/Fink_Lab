8bde3bcdce48588d78e04381b97bd4db29b0d042
AshleyHsieh123 <ashley.hsieh@northwestern.edu>
2025-04-01 16:22:47 -0500

    Update test_midline.py

--- test_midline.py
+++ test_midline.py
@@ -588,11 +588,9 @@ def update_figure_1():
     HistoSubplot(RightEarBarInitial,'Right ear bar',2,2,'mm',mouseData1[0][2])
     HistoSubplot(NoseDVposition,'Nose DV position',2,3,'˚',mouseData1[0][3])
     HistoSubplot(RCSlambdaDistance,'RCS - lambda distance',2,4,'µm',mouseData1[0][4])
-
-    plt.text(-5, 60, 'midline', fontsize = 22)
     
     fig1.suptitle(f'Data for mouse {mouse_id}', fontweight="bold", y = 1)
-    plt.tight_layout()
+    plt.tight_layout(rect=[0, 0, 1, 0.98])
 
     # Convert plot to image and display in result box
     img_buf1 = BytesIO()
